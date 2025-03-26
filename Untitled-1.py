2636c341bda9739d6b318c6860c8dda73ac4f30d
DeusBigelow <madroscoe@icloud.com>
2025-03-26 15:01:30 -0500

    Test Fail

---
+++ Untitled-1.py
@@ -0,0 +1,37 @@
+class clientAgent():
+    def __init__(self, ClientName, ClNumber, ClAge):
+        self.ClientName = ""
+        self.ClNumber = 0
+        self.ClAge = 00/00/0000
+               
+    
+    def __repr__(self):
+        return f"Client Name: {self.ClientName},/n Client Number: {self.ClNumber},/n Client Age: {self.ClAge}"
+    
+
+class clientAgentList(self,clientAgent):
+     def __init__(self.clientAgent):
+           self.head = None
+           
+
+
+
+
+
+
+
+
+
+#class clientAccount():
+ 
+ #   def __init__(self, clientAgent):
+  #        self.adjudgeName = actor
+   #       self.selfClList = actor
+    #      self.searchClient = actor
+     #     self.deleteClient = actor
+      #    self.quit = actor
+    #def __init__(self, adjudgeName):
+     #   self.adjudgeName = str(input("Enter Client Name: "))
+      #  return f"Client Name: {self.adjudgeName}"
+    #def __init__(self, selfClList):
+     #     return print(clientAgent(actor))
